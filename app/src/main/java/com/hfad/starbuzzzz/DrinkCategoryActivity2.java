ff241e8164b9a9b9a5f67d0c542a46184ea076cf
Saul <saulftopete2@gmail.com>
2018-11-15 09:27:19 -0800

    starbuzzzzzzzż

--- app/src/main/java/com/hfad/starbuzzzz/DrinkCategoryActivity2.java
+++ app/src/main/java/com/hfad/starbuzzzz/DrinkCategoryActivity2.java
@@ -1,7 +1,10 @@
 package com.hfad.starbuzzzz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 public class DrinkCategoryActivity2 extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class DrinkCategoryActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
+
+        AdapterView.OnClickListener itemClickListener = new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> listView, View itemView, int position, long id){
+                if(position == 0){
+                    Intent intent = new Intent(TopLevelActivty1.this, DrinkCategoryActivity2.class);
+                    startActivity(intent);
+                }
+            }
+        };
     }
+
+
 }
